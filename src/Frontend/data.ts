b7cde0c423c97ce495f65a776abf1e4083e6804a
Nico <akebra77@gmail.com>
2025-03-27 07:56:17 +0100

    fixing PaymentController & Data Img

--- src/Frontend/data.ts
+++ src/Frontend/data.ts
@@ -3,7 +3,7 @@ import {ItemData} from "./components/CartItem.tsx";
 export const Products: ItemData[] = [
     {
         description: "Premium Shoes",
-        image: "https://source.unsplash.com/NUoPWImmjCU",
+        image: "https://images.unsplash.com/photo-1608231387042-66d1773070a5?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         name: "Puma Shoes",
         price: 20,
         quantity: 1,
@@ -11,7 +11,7 @@ export const Products: ItemData[] = [
     },
     {
         description: "Comfortable everyday slippers",
-        image: "https://source.unsplash.com/K_gIPI791Jo",
+        image: "https://images.unsplash.com/photo-1739138054456-0aca1b90e4a9?q=80&w=3472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         name: "Nike Sliders",
         price: 10,
         quantity: 1,
--- src/backend/src/main/java/com/nicoq/stripe_java/backend/PaymentController.java
+++ src/backend/src/main/java/com/nicoq/stripe_java/backend/PaymentController.java
@@ -12,17 +12,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @CrossOrigin
 public class PaymentController {
 
-    String STRIPE_API_KEY = System.getenv().get("STRIPE_API_KEY");
+    String STRIPE_API_KEY = System.getenv("STRIPE_API_KEY");
 
     @PostMapping("/checkout/hosted")
     String hostedCheckout(@RequestBody RequestDTO requestDTO) throws StripeException {
-      return "Hello World!";
+
+        Stripe.apiKey = STRIPE_API_KEY;
+        String clientBaseURL = System.getenv("CLIENT_BASE_URL");
+
+        if (clientBaseURL == null) {
+            throw new IllegalArgumentException("CLIENT_BASE_URL is not set in environment variables.");
+        }
+
+        Customer customer = CustomerUtil.findOrCreateCustomer(requestDTO.getCustomerEmail(), requestDTO.getCustomerName());
+
+        SessionCreateParams.Builder paramsBuilder =
+                SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setCustomer(customer.getId())
+                        .setSuccessUrl(clientBaseURL + "/success?session_id={CHECKOUT_SESSION_ID}")
+                        .setCancelUrl(clientBaseURL + "/failure");
+
+        for (Product product : requestDTO.getItems()) {
+            paramsBuilder.addLineItem(
+                    SessionCreateParams.LineItem.builder()
+                            .setQuantity(1L)
+                            .setPriceData(
+                                    PriceData.builder()
+                                            .setProductData(
+                                                    PriceData.ProductData.builder()
+                                                            .putMetadata("app_id", product.getId())
+                                                            .setName(product.getName())
+                                                            .build()
+                                            )
+                                            .setCurrency(ProductDAO.getProduct(product.getId()).getDefaultPriceObject().getCurrency())
+                                            .setUnitAmountDecimal(ProductDAO.getProduct(product.getId()).getDefaultPriceObject().getUnitAmountDecimal())
+                                            .build())
+                            .build());
+        } 
+
+        Session session = Session.create(paramsBuilder.build());
+
+        return session.getUrl();
     }
-
 }
-
